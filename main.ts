8dc22954ca98499df6c0cf8750b0acbe8f96f146
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-10-03 13:27:07 -0400

    Turns an LED on and off

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matheus
+ * Created on: Sep 2025
+ * This program turns an LED on and off
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // This turns on a LED
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    // This turns off a LED
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
